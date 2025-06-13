b757268a320a167df5d96115097f82752d9bdc2c
hoonbeom <hoonbeom.dev@gmail.com>
2025-06-13 16:04:49 +0900

    ✨ Feat: GET요청 캐시기능 추가

---
+++ google-app-script.js
@@ -0,0 +1,48 @@
+function doGet(e) {
+    const parameter = e.parameter;
+    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(parameter.sheet);
+    const fields = sheet.getRange(1, 2, 1, sheet.getLastColumn()).getValues()[0].filter(Boolean);
+
+    if (parameter.method === 'POST') {
+        const values = fields.map((field) => parameter[field]);
+        const id = sheet.getLastRow();
+        sheet.appendRow([id, ...values]);
+
+        return ContentService.createTextOutput(JSON.stringify({ success: true })).setMimeType(ContentService.MimeType.JSON);
+    }
+
+    if (parameter.method === 'GET') {
+        const data = sheet.getDataRange().getValues();
+        const headers = data[0];
+        const rows = data.slice(1);
+        const result = rows.map((row) =>
+            headers.reduce((p, field, index) => {
+                p[field] = row[index];
+                return p;
+            }, {})
+        );
+
+        return ContentService.createTextOutput(JSON.stringify(result)).setMimeType(ContentService.MimeType.JSON);
+    }
+
+    if (parameter.method === 'PATCH') {
+        const id = Number(parameter.id);
+        fields.forEach((field, j) => {
+            if (typeof parameter[field] !== 'undefined') {
+                sheet.getRange(id + 1, j + 2).setValue(parameter[field]);
+            }
+        });
+
+        return ContentService.createTextOutput(JSON.stringify({ success: true })).setMimeType(ContentService.MimeType.JSON);
+    }
+
+    if (parameter.method === 'PUT') {
+        const id = Number(parameter.id);
+        const values = fields.map((field) => parameter[field]);
+        sheet.getRange(id + 1, 1, 1, values.length + 1).setValues([[id, ...values]]);
+
+        return ContentService.createTextOutput(JSON.stringify({ success: true })).setMimeType(ContentService.MimeType.JSON);
+    }
+
+    return ContentService.createTextOutput(JSON.stringify({ error: 'Invalid method' })).setMimeType(ContentService.MimeType.JSON);
+}
--- src/lib/google-sheet.ts
+++ src/lib/google-sheet.ts
@@ -1,17 +1,28 @@
+
 class GoogleSheet {
     url;
 
+    private _cache = new Map<string, { data: unknown }>();
+
+
+    /**
+     * @param url Google App Script Url
+     */
     constructor({ url }: { url: string }) {
         this.url = url;
-    }
 
+    }
     /**
      * Select
      */
     async GET({ sheet }: { sheet: string }) {
-        const r = await fetch(`${this.url}?method=GET&sheet=${sheet}`);
-        const json = await r.json();
-        return json;
+        const key = `GET:${sheet}`;
+        const cached = this._cache.get(key);
+        if (cached?.data) return cached.data;
+        const encodeURI = encodeURIComponent(`method=GET&sheet=${sheet}`)
+        const r = await fetch(`${this.url}?${encodeURI}`).then(r => r.json());
+        this._cache.set(key, { data: r });
+        return r;
     }
 
     /**
@@ -19,9 +30,10 @@ class GoogleSheet {
      */
     async POST<T extends Record<string, string>>({ sheet, ...body }: T & { sheet: string }) {
         const queryString = new URLSearchParams(body).toString();
-        const r = await fetch(`${this.url}?method=POST&sheet=${sheet}&${queryString}`);
-        const json = await r.json();
-        return json;
+        const encodeURI = encodeURIComponent(`method=POST&sheet=${sheet}&${queryString}`)
+        const r = await fetch(`${this.url}?${encodeURI}`).then(r => r.json());
+        this._invalidCache(sheet);
+        return r;
     }
 
     /**
@@ -29,9 +41,10 @@ class GoogleSheet {
      */
     async PUT<T extends Record<string, string>>({ sheet, id, ...body }: T & { sheet: string; id: string }) {
         const queryString = new URLSearchParams(body).toString();
-        const r = await fetch(`${this.url}?method=PUT&sheet=${sheet}&id=${id}&${queryString}`);
-        const json = await r.json();
-        return json;
+        const encodeURI = encodeURIComponent(`method=PUT&sheet=${sheet}&id=${id}&${queryString}`);
+        const r = await fetch(`${this.url}?${encodeURI}`).then(r => r.json());
+        this._invalidCache(sheet);
+        return r;
     }
 
     /**
@@ -39,10 +52,16 @@ class GoogleSheet {
      */
     async PATCH<T extends Record<string, string>>({ sheet, id, ...body }: T & { sheet: string; id: string }) {
         const queryString = new URLSearchParams(body).toString();
-        const r = await fetch(`${this.url}?method=PATCH&sheet=${sheet}&id=${id}&${queryString}`);
-        const json = await r.json();
-        return json;
+        const encodeURI = encodeURIComponent(`method=PATCH&sheet=${sheet}&id=${id}&${queryString}`);
+        const r = await fetch(`${this.url}?${encodeURI}`).then(r => r.json());
+        this._invalidCache(sheet);
+        return r;
     }
+
+    private _invalidCache(sheet: string) {
+        this._cache.delete(`GET:${sheet}`);
+    }
+
 }
 
 export default GoogleSheet;
