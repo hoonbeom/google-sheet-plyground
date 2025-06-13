372dafa2d3f7cb2cbac3e8a31b031b1a3b2ff401
hoonbeom <hoonbeom.dev@gmail.com>
2025-06-13 17:18:53 +0900

    🐛 Fix: google-sheet 캐시 기능 삭제

--- src/lib/google-sheet.ts
+++ src/lib/google-sheet.ts
@@ -2,8 +2,6 @@
 class GoogleSheet {
     url;
 
-    private _cache = new Map<string, { data: unknown }>();
-
     /**
      * @param url Google App Script Url
      */
@@ -14,11 +12,7 @@ class GoogleSheet {
      * Select
      */
     async GET({ sheet }: { sheet: string }) {
-        const key = `GET:${sheet}`;
-        const cached = this._cache.get(key);
-        if (cached?.data) return cached.data;
         const r = await fetch(`${this.url}?method=GET&sheet=${sheet}`).then(r => r.json());
-        this._cache.set(key, { data: r });
         return r;
     }
 
@@ -28,7 +22,6 @@ class GoogleSheet {
     async POST<T extends Record<string, string>>({ sheet, ...body }: T & { sheet: string }) {
         const queryString = new URLSearchParams(body).toString();
         const r = await fetch(`${this.url}?method=POST&sheet=${sheet}&${queryString}`).then(r => r.json());
-        this._invalidCache(sheet);
         return r;
     }
 
@@ -38,7 +31,6 @@ class GoogleSheet {
     async PUT<T extends Record<string, string>>({ sheet, id, ...body }: T & { sheet: string; id: string }) {
         const queryString = new URLSearchParams(body).toString();
         const r = await fetch(`${this.url}?method=PUT&sheet=${sheet}&id=${id}&${queryString}`).then(r => r.json());
-        this._invalidCache(sheet);
         return r;
     }
 
@@ -48,13 +40,10 @@ class GoogleSheet {
     async PATCH<T extends Record<string, string>>({ sheet, id, ...body }: T & { sheet: string; id: string }) {
         const queryString = new URLSearchParams(body).toString();
         const r = await fetch(`${this.url}?method=PATCH&sheet=${sheet}&id=${id}&${queryString}`).then(r => r.json());
-        this._invalidCache(sheet);
         return r;
     }
 
-    private _invalidCache(sheet: string) {
-        this._cache.delete(`GET:${sheet}`);
-    }
+
 
 }
 
